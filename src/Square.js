e70e51c4b95d90ffd0d8f6e7454cb9963efbf5df
erica2605@live.com <erica2605@live.com>
2019-04-04 11:43:40 -0700

    user can now click squares to fill them

--- src/Square.js
+++ src/Square.js
@@ -1,23 +1,17 @@
 import React from 'react'
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
-    render() {
-      return (
-        <button 
-          className="square"
-          onClick={() => this.setState({value: 'X'})}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+}
 
   export default Square
 
