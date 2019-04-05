ccc4260b5426725b5512267d50c2490ef6dda151
erica2605@live.com <erica2605@live.com>
2019-04-04 17:31:26 -0700

    game working

--- src/Board.js
+++ src/Board.js
@@ -9,27 +9,17 @@ class Board extends React.Component {
       xIsNext: true,
     };
   }
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
   render() {
-    const status = 'Next player: X';
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
